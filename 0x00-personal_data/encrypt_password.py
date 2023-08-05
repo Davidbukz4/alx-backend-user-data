c3f037ac0256e3d846fa0fdc9a759d1c6b7b250a
David Egwuatu <davidegwuatu2@gmail.com>
2023-08-05 09:36:58 +0000

    added task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,5 +9,10 @@ from bcrypt import hashpw
 def hash_password(password: str) -> bytes:
     ''' encrypts password and returns the password in bytes '''
     pwd = password.encode()
-    hashed_pwd = hashpw(pwd, gensalt())
+    hashed_pwd = hashpw(pwd, bcrypt.gensalt())
     return hashed_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' check valid password '''
+    return bcrypt.checkpw(password.encode(), hashed_password)
