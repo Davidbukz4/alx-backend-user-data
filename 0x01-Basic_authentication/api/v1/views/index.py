2b794f8be7309f5e3f07ce2bd4f3bcae0c6e92ee
David Egwuatu <davidegwuatu2@gmail.com>
2023-08-08 05:18:09 +0000

    added task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized() -> str:
     """ Unauthorized Error Handler
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ Forbidden Error Handler
+    """
+    abort(403)
