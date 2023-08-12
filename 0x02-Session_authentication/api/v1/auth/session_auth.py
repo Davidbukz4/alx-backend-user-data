1adc6dbd71b8ce356f967a499a04f2743be99bb7
David Egwuatu <davidegwuatu2@gmail.com>
2023-08-12 03:05:10 +0000

    added task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,3 +17,9 @@ class SessionAuth(Auth):
             self.user_id_by_session_id[self.session_id] = user_id
             return self.session_id
         return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        ''' returns a user ID based on a session ID '''
+        if session_id and isinstance(session_id, str):
+            return self.user_id_by_session_id.get(self.session_id)
+        return None
