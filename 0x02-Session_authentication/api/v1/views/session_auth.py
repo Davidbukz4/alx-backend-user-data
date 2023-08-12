ef0989e6abbf44d79b1a1853ab408f835cf55cf7
David Egwuatu <davidegwuatu2@gmail.com>
2023-08-12 04:23:36 +0000

    resolved task 7 error(i)

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -5,6 +5,7 @@ Session Authentication View
 from api.v1.views import app_views
 from flask import request, jsonify, abort
 from models.user import User
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
