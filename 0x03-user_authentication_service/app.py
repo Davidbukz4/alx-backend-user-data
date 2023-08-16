d97f3cadc7df6a48582d45be881565791c7ce7cb
David Egwuatu <davidegwuatu2@gmail.com>
2023-08-16 05:39:12 +0000

    added task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 '''
 Basic Flask App
 '''
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -30,7 +30,7 @@ def users():
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login() -> str:
+def login():
     ''' user login '''
     email = request.form.get('email')
     password = request.form.get('password')
@@ -42,6 +42,20 @@ def login() -> str:
         response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie('session_id', session_id)
     return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    ''' logout user '''
+    session_id = request.cookies.get('session_id', None)
+    if session_id is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
